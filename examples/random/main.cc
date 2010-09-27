1dc6f5d59e5bf35193ed6f057d488395a2fc96de
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2010-09-27 22:14:39 +0200

    Fix record example includes.

--- examples/random/main.cc
+++ examples/random/main.cc
@@ -16,6 +16,8 @@
 // License along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+#include <cstdlib>
+
 // Dige includes.
 #include <dige/window.h>
 #include <dige/image.h>
