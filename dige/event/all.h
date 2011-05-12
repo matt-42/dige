bca40db2377f5b6cdeabd1b484e63697888a1577
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2011-05-12 22:52:50 +0200

    Add all.h files.

---
+++ dige/event/all.h
@@ -0,0 +1,44 @@
+// Copyright (C) 2010 Matthieu Garrigues
+//
+// This file is part of dige.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+/*!
+**\file   event/all.h
+**\author Matthieu Garrigues <matthieu.garrigues@gmail.com>
+**\date   Thu May 12 22:50:26 2011
+**
+**\brief  Include all event headers.
+**
+**
+*/
+
+#ifndef DIGE_EVENT_ALL_H_
+# define DIGE_EVENT_ALL_H_
+
+# include <dige/event/click.h>
+# include <dige/event/custom_events.h>
+# include <dige/event/event.h>
+# include <dige/event/event_queue.h>
+# include <dige/event/event_set.h>
+# include <dige/event/event_waiter.h>
+# include <dige/event/keycode.h>
+# include <dige/event/key_release.h>
+# include <dige/event/make_event.h>
+# include <dige/event/mouse_move.h>
+# include <dige/event/or_event.h>
+# include <dige/event/wait.h>
+
+#endif
---
+++ dige/widgets/all.h
@@ -0,0 +1,38 @@
+// Copyright (C) 2010 Matthieu Garrigues
+//
+// This file is part of dige.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+/*!
+**\file   widgets/all.h
+**\author Matthieu Garrigues <matthieu.garrigues@gmail.com>
+**\date   Thu May 12 21:48:59 2011
+**
+**\brief  Include all widgets header.
+**
+**
+*/
+
+#ifndef DIGE_WIDGETS_ALL_H_
+# define DIGE_WIDGETS_ALL_H_
+
+# include <dige/widgets/gl_widget.h>
+# include <dige/widgets/image_view.h>
+# include <dige/widgets/push_button.h>
+# include <dige/widgets/slider.h>
+# include <dige/widgets/tracer.h>
+# include <dige/widgets/tracer_view.h>
+
+#endif
