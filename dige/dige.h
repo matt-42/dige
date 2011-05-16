6a5a17e2ec3f23c22d0555be73a52ad9cc55b031
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2011-05-16 23:09:55 +0200

    Add dige.h which include all headers of the library.

---
+++ dige/dige.h
@@ -0,0 +1,49 @@
+// Copyright (C) 2011 Matthieu Garrigues
+//
+// This file is part of dige.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+#ifndef DIGE_ALL_HEADERS_H_
+# define DIGE_ALL_HEADERS_H_
+
+# include <dige/abstract_texture.h>
+# include <dige/color_picker_control.h>
+# include <dige/color_picker.h>
+# include <dige/dige.h>
+# include <dige/displaylist.h>
+# include <dige/error.h>
+# include <dige/image.h>
+# include <dige/internal_texture.h>
+# include <dige/is_texture_type.h>
+# include <dige/keywords.h>
+# include <dige/named_object.h>
+# include <dige/need_qapp.h>
+# include <dige/panzoom_control.h>
+# include <dige/panzoom.h>
+# include <dige/pause.h>
+# include <dige/pick_coords.h>
+# include <dige/point2d.h>
+# include <dige/process_events.h>
+# include <dige/recorder.h>
+# include <dige/rect2d.h>
+# include <dige/singleton.h>
+# include <dige/texture.h>
+# include <dige/ui_layout.h>
+# include <dige/value_traits.h>
+# include <dige/window.h>
+# include <dige/window_placer.h>
+
+#endif
