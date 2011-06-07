637b13714392a8f6836df5a3fd1120713cfa2826
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2011-06-07 23:34:20 +0200

    Improve simple_image documentation.
    modified:   examples/simple_image/main.cc

--- examples/simple_image/main.cc
+++ examples/simple_image/main.cc
@@ -19,7 +19,6 @@
 #include <cstdlib>
 #include <ctime>
 
-// Dige includes.
 #include <dige/dige.h>
 
 // Our simple rgb image type
@@ -44,16 +43,29 @@ struct rgb_image
   unsigned char* data;
 };
 
-
 namespace dg
 {
 
-  // Adapt our rgb_image type into the dige image type.
+  // We then define the bridge between our custom rgb_image type and the
+  // internal dg::image type. This will be called before each display of an
+  // rgb_image. image takes to template parameters:
+  //
+  // -> The format of the pixels which can be : - trait::format::rgb
+  //                                            - trait::format::rgba
+  //                                            - trait::format::luminance
+  //     Or, if your version of opengl support it:
+  //                                            - trait::format::bgr
+  //                                            - trait::format::bgra
+  // -> The data type which can be: float, char, short, int,
+  //    unsigned float, unsigned char, unsigned short, unsigned int.
+  // Note that when displaying float, values has to be normalized between
+  // 0.f and 1.f.
   image<trait::format::rgb, unsigned char>
   adapt(const ::rgb_image& i)
   {
     return image<trait::format::rgb, unsigned char>
       (i.width, i.height, i.data);
+    // Warning: dg::image only handle contiguous buffers of pixel.
   }
 
 }
@@ -67,18 +79,20 @@ int main()
   srand(time(0));
   rgb_image img(20, 20);
 
+  // Create a window containing an ImageView named random that will
+  // display our rgb_image.
   dg::Window("simple_image", 500, 300) <<= ImageView("random");
 
   unsigned t = clock();
-  while (clock() - t < 5 * CLOCKS_PER_SEC)
+  while (clock() - t < 2 * CLOCKS_PER_SEC)
   {
     for (unsigned i = 0; i < img.image_size; i++)
       img.data[i] = rand();
 
-    // Display img in display("random"). dl() starts a list of images.
+    // Display img in the ImageView. dl() starts a list of images.
     ImageView("random")  <<= dl() - img;
   }
 
-  // Wait for any key release event.
-  dg::event::wait(key_release());
+  // Wait for any key press event.
+  wait_event(key_press());
 }
